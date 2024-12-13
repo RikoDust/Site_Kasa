1aec119433a56153cbf8398bdf3aa4e11b70d148
Emeric <emeric.pataut@gmail.com>
2024-12-13 16:52:35 +0100

    feat(header): add header component and its style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,26 @@
-import '../styles/header.css'
+import { useState } from "react"; // Import de useState
+import '../styles/header.css';
+import logo from "../assets/images/logo.png";
 
 function Header() {
+    const [activeLink, setActiveLink] = useState("Accueil");
+
     return (
-      <header>
-        <h2>Ceci est mon header</h2>
+      <header className='header'>
+        <div className='logo'>
+            <img src={logo} alt="Logo_Kasa" />
+        </div>
+        <nav className="nav">
+            <a href="#home"
+            className={`nav-link ${activeLink === "Accueil" ? "active" : ""}`} 
+            onClick={() => setActiveLink("Accueil")}>Accueil</a>
+            <a href="#about"
+            className={`nav-link ${activeLink === "A Propos" ? "active" : ""}`}
+            onClick={() => setActiveLink("A Propos")}>A Propos</a>
+        </nav>
       </header>
     );
-  }
+  };
   
 
 
