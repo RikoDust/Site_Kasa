1a808a82c12651fff12afe33666cf3a7bd12d77d
Emeric <emeric.pataut@gmail.com>
2024-12-13 19:18:13 +0100

    feat(routes): install React Router, setup Routes.jsx and page management

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
+import { BrowserRouter as Router } from 'react-router-dom';
 import Header from './components/Header';
+import RoutesComponent from './Routes';
 import './styles/App.css'
 
 
 function App() {
   return (
+    <Router>
       <div>
           <Header />
+          <RoutesComponent />
       </div>
-
+    </Router>
   );
-};
+}
+
+
 
 
 export default App
