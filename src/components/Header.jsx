5b5e4f8dead55057031227659c7ace867d89ba15
Emeric <emeric.pataut@gmail.com>
2024-12-13 10:29:36 +0100

    refactor(structure): organization of CSS, component, and data files

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import '../styles/header.css'
+
+function Header() {
+    return (
+      <header>
+        <h2>Ceci est mon header</h2>
+      </header>
+    );
+  }
+  
+
+
+
+  export default Header
